a9e1bb3d6dcab5f5ab0c15e036cf090730b66f0f
Xen0w <benazouz.souheilabdessamed@gmail.com>
2026-04-01 18:20:30 +0200

    samado

---
+++ pyradar/network.py
@@ -0,0 +1,5 @@
+import socket, ipaddress, platform, subprocess, re
+
+get_local_ip_and_mask()
+mask_to_cidr()
+parse_network()
---
+++ pyradar/scanner.py
@@ -0,0 +1,6 @@
+import subprocess, platform, re, concurrent.futures
+from pyradar.network import parse_network  # it will need this later
+
+
+ping()
+scan_network()
